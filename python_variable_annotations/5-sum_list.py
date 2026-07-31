1f353d09af991ac963bc2771a5f2c064c71d3074
Venghour-holberton-git <13067@holbertonstudents.com>
2026-07-31 23:29:36 +1000

    f

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,7 +3,8 @@
 """function module"""
 
 
-def sum_list(input_list: list[float]) -> float:
+from typing import List
+def sum_list(input_list: List[float]) -> float:
     res: float = 0.0
     for n in input_list:
         res += n
