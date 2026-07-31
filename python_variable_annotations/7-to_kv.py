98fb00990625f2f091ab6aaf10bf33f61cc9876c
Venghour-holberton-git <13067@holbertonstudents.com>
2026-07-31 23:55:00 +1000

    f

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,8 +3,8 @@
 """function module"""
 
 
-from typing import List, Union
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     return (k, v*v)
