c49dc32f637b1ded3d830f8c3a506c48db492d40
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-02 01:27:46 +1000

    f

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Module containing the wait_n coroutine."""
 
-import asyncio
 import time
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
@@ -12,6 +12,3 @@ async def measure_time(n: int, max_delay: int) -> float:
     await wait_n(n, max_delay)
     end_time = time.perf_counter()
     return (end_time - start_time) / n
-
-if __name__ == "__main__":
-    print(asyncio.run(measure_time(3, 4)))
