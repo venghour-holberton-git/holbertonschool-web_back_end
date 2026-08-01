32d5a716674462157af820b17a812ba3dd596d76
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-02 09:40:14 +1000

    f

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,12 +9,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """This function measure time it takes to excecute all async func"""
     start_time: float = time.perf_counter()
-    await asyncio.gather(
-                        async_comprehension(),
-                        async_comprehension(),
-                        async_comprehension(),
-                        async_comprehension()
-                        )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time: float = time.perf_counter()
     return end_time - start_time
 
