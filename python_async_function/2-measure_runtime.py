82a29a51daecc52aef0af8f2cdb947397f6c0b76
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-02 07:14:10 +1000

    f

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,7 +8,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 async def measure_time(n: int, max_delay: int) -> float:
     """Spawn wait_random n times and return the delays in ascending order."""
-    start_time = time.perf_counter()
+    start_time:float = time.perf_counter()
     await wait_n(n, max_delay)
-    end_time = time.perf_counter()
-    return (end_time - start_time) / n
+    end_time:float = time.perf_counter()
+    total_time:float = end_time - start_time
+    return total_time / n
