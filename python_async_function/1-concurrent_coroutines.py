9f71c687ba2044b2f83aed226077a6490868c232
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-02 01:01:15 +1000

    f

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,7 +11,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """This function get n and max delay"""
+    """This function get n - number of coroutines and max delay"""
     float_list: List[float] = []
     for i in range(n):
         float_list.append(await wait_random(max_delay))
