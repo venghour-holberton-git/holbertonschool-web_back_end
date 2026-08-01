3ac6b767b214fdc14cc3ea0b2732ab1295470b72
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-02 00:29:57 +1000

    f

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""function module"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int) -> float:
+    wait_time: float = random.uniform(0, max_delay)
+    await asyncio.sleep(wait_time)
+    return wait_time
+
+if __name__ == "__main__":
+    print(asyncio.run(wait_random(5)))
