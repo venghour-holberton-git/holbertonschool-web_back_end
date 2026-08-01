4b0091ed111f56eef2e7a36ce9ae6e20e7a81ef9
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-02 00:31:06 +1000

    f

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,6 +8,7 @@ import random
 
 
 async def wait_random(max_delay: int) -> float:
+    """This funtion take in random value, wait then return the value back"""
     wait_time: float = random.uniform(0, max_delay)
     await asyncio.sleep(wait_time)
     return wait_time
