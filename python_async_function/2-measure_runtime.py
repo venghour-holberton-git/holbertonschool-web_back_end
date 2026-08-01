5a171b407cb2307570ec596ae7d4c469f44d526f
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-02 07:26:59 +1000

    f

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-"""Module containing the wait_n coroutine."""
+"""Module for measuring the runtime of wait_n."""
 
 import time
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
-    """Measure the average execution time of wait_n."""
-    start_time: float = time.perf_counter()
+    start = time.perf_counter()
     await wait_n(n, max_delay)
-    end_time: float = time.perf_counter()
-    total_time: float = end_time - start_time
-    return total_time / n
+    end = time.perf_counter()
+
+    return (end - start) / n
