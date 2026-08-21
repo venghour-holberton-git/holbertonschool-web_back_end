f0e917211ca599c24c1693b04bedf6915721911c
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-21 20:48:22 +1000

    f

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+"""
+    This is a function module
+"""
 
 def index_range(page, page_size):
+    """
+        This function return a tuple of start index and end index
+    """
     start_index = (page - 1) * page_size
     end_index = page * page_size
 
