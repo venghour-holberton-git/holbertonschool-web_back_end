735f935a1a33cc3dd87fdd64f6f6b7d0495ae01a
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-21 21:14:24 +1000

    f

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+    This is a class module
+"""
+
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            assert page > 0
+            assert page_size > 0
+            data = self.dataset()
+            selected_range = self.index_range(page, page_size)
+            return data[selected_range[0]: selected_range[1]]
+
+    def index_range(self, page, page_size):
+        """
+            This function return a tuple of start index and end index
+        """
+        start_index = (page - 1) * page_size
+        end_index = page * page_size
+
+        return (start_index, end_index)
+
+if __name__ == "__main__":
+     server = Server()
+     data = server.get_page(2, 5)
+     print(data)
