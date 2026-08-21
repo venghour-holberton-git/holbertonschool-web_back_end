10b3928e4c884b1afee9eeb182f129b0bbdd831f
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-21 20:45:07 +1000

    f

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def index_range(page: int, page_size: int):
+def index_range(page: int, page_size: int) -> tuple:
     return ((page - 1) * page_size, page * page_size)
 
 
