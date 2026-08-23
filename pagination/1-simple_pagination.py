7212cc6094d750e94a32c1a98a965afb98bec72b
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-24 02:34:34 +1000

    f

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
-"""Module for hypermedia pagination."""
+"""
+    This is a class module
+"""
+
 
 import csv
 import math
-from typing import List, Tuple, Dict, Union
+from typing import List
 
+def index_range(self, page, page_size):
+    """
+        Return a tuple of start index and end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return the start and end indexes for a pagination range."""
-    start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
-
+    return (start_index, end_index)
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initialize the server dataset cache."""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Return the cached dataset."""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -32,34 +36,12 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1,
-                 page_size: int = 10) -> List[List]:
-        """Return the requested page of the dataset."""
-        assert isinstance(page, int) and page > 0, "Must be int and > 0"
-        assert isinstance(page_size, int) and page_size > 0, \
-            "Must be int > 0"
-
-        (start, end) = index_range(page, page_size)
-
-        return self.dataset()[start:end]
-
-    def get_hyper(
-            self, page: int = 1, page_size: int = 10) -> Dict[
-                str, Union[int, List, None]]:
-        """Return pagination data and hypermedia metadata."""
-        data = self.get_page(page, page_size)
-
-        total_pages = math.ceil(
-            len(self.dataset()) / page_size) if self.dataset() else 0
-
-        next_page = page + 1 if page < total_pages else None
-        prev_page = page - 1 if page > 1 else None
-
-        return {
-            'page_size': len(data),
-            'page': page,
-            'data': data,
-            'next_page': next_page,
-            'prev_page': prev_page,
-            'total_pages': total_pages,
-        }
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+            Return a List of data from the start index to end index
+        """
+        assert page > 0
+        assert page_size > 0
+        data = self.dataset()
+        selected_range = index_range(page, page_size)
+        return data[selected_range[0]: selected_range[1]]
