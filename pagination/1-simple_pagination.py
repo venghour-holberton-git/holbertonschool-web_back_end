3027338ded53743d4377793d2234c766030840e5
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-24 02:37:21 +1000

    f

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Module for hypermedia pagination."""
+"""Module for simple pagination."""
 
 import csv
 import math
-from typing import List, Tuple, Dict, Union
+from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -42,24 +42,3 @@ class Server:
         (start, end) = index_range(page, page_size)
 
         return self.dataset()[start:end]
-
-    def get_hyper(
-            self, page: int = 1, page_size: int = 10) -> Dict[
-                str, Union[int, List, None]]:
-        """Return pagination data and hypermedia metadata."""
-        data = self.get_page(page, page_size)
-
-        total_pages = math.ceil(
-            len(self.dataset()) / page_size) if self.dataset() else 0
-
-        next_page = page + 1 if page < total_pages else None
-        prev_page = page - 1 if page > 1 else None
-
-        return {
-            'page_size': len(data),
-            'page': page,
-            'data': data,
-            'next_page': next_page,
-            'prev_page': prev_page,
-            'total_pages': total_pages,
-        }
