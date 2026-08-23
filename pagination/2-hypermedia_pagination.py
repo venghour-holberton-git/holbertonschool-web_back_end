ed09315922f663fb0266403782b208854f854861
Venghour-holberton-git <13067@holbertonstudents.com>
2026-08-24 02:45:42 +1000

    f

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -3,7 +3,6 @@
     This is a class module
 """
 
-
 import csv
 import math
 from typing import List
@@ -32,11 +31,29 @@ class Server:
         """
             Return a List of data from the start index to end index
         """
-        assert page > 0
-        assert page_size > 0
+        assert isinstance(page, int) and page > 0, "Must be int and > 0"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "Must be int > 0"
+
         data = self.dataset()
         selected_range = self.index_range(page, page_size)
-        return data[selected_range[0]: selected_range[1]]
+        return data[selected_range[0]:selected_range[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10):
+        """
+            Return pagination information for a page
+        """
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
 
     def index_range(self, page, page_size):
         """
@@ -46,9 +63,3 @@ class Server:
         end_index = page * page_size
 
         return (start_index, end_index)
-
-
-if __name__ == "__main__":
-    server = Server()
-    data = server.get_page(2, 5)
-    print(data)
